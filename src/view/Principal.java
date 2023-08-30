7e7453a3a783d47f249850de8fa66f4e033e97b2
leope <leope@DESKTOP-5CB9BEC>
2023-08-30 16:12:37 -0300

    commit

---
+++ src/view/Principal.java
@@ -0,0 +1,15 @@
+package view;
+
+public class Principal {
+
+
+	public static void main(String[] args) {
+		int posicoes = 1, contador = 0;
+		while(true){
+			posicoes *= 1000;
+			System.out.println(++contador + ": " + posicoes);
+			String[] vetor = new String[posicoes];
+		}
+	}
+
+}
